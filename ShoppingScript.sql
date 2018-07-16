d91df70a6db542d6f8c8a9efa191fa30e8fa6e0e
Bakhary Drummond <bdrummond3625@bths.edu>
2018-07-15 22:54:53 -0400

    Building DB Tables

--- ShoppingScript.sql
+++ ShoppingScript.sql
@@ -17,24 +17,38 @@ GO
 
 Begin Transaction;
 
-CREATE TABLE Product
-(
-
-);
-
 CREATE TABLE Category 
 (
+	id				integer					identity(1,1), 
+	name			varchar(30),
 
+	Constraint pk_category Primary Key (id)
 );
 
-CREATE TABLE User
+CREATE TABLE Product
 (
+	id				integer				identity(1,1),
+	name			varchar(30)			Not Null,
+	description		varchar(max)		Not Null	default 'No description is available for this product',
+	category_id		integer				Not Null,
+	Is_BestSeller	bit,
+	cost			money,				
 
+	Constraint pk_product Primary Key (id),
+	Constraint fk_product_category Foreign Key (category_id) References Category (id)
+);
+
+
+CREATE TABLE Customer
+(
+	id int identity(1,1), 
+	
 );
 
 CREATE TABLE Sale
 (
-
+	id int identity(1,1), 
+	
 );
 
 CREATE TABLE Recent_Purchases 
